7555d7ae29b91d05752c4e1fb3bf31a131ef4aa9
Adele Lynn <virepri2k@gmail.com>
2017-12-11 17:12:06 -0800

    10_2. Fuck 10_2.

--- src/days/FunList.kt
+++ src/days/FunList.kt
@@ -21,7 +21,7 @@ val list = listOf(
         ::day7p1, ::day7p2,
         ::day8p1, ::day8p2,
         ::day9p1, ::day9p2,
-        ::day10p1
+        ::day10p1, ::day10p2
 )
 
 val tests = listOf(
@@ -34,7 +34,7 @@ val tests = listOf(
         listOf("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"), listOf("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)"),
         listOf("b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10"), listOf("b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10"),
         listOf("{}", "{{{}}}", "{{},{}}", "{{{},{},{{}}}}", "{<a>,<a>,<a>,<a>}", "{{<ab>},{<ab>},{<ab>},{<ab>}}", "{{<!!>},{<!!>},{<!!>},{<!!>}}", "{{<a!>},{<a!>},{<a!>},{<ab>}}"), listOf("<>", "<random characters>", "<<<<>", "<{!>}>", "<!!>", "<!!!>>", "<{o\"i!a,<{i<a>"),
-        listOf("3, 4, 1, 5")
+        listOf("3, 4, 1, 5"), listOf("", "AoC 2017", "1,2,3", "1,2,4")
 )
 
 val answers = listOf(
@@ -47,5 +47,5 @@ val answers = listOf(
         listOf("tknk"), listOf("60"),
         listOf("1"), listOf("10"),
         listOf("1", "6", "5", "16", "1", "9", "9", "3"), listOf("0", "17", "3", "2", "0", "0", "10"),
-        listOf("12")
+        listOf("12"), listOf("a2582a3a0e66e6e86e3812dcb672a272", "33efeb34ea91902bb2f59c9920caa6cd", "3efbe78a8d82f29979031a4aa0b16a9d", "63960835bcdc130f0b66d7ff4f6a5a8e")
 )
--- src/days/day10/Day10.kt
+++ src/days/day10/Day10.kt
@@ -1,17 +1,46 @@
 package days.day10
 
+import kotlin.experimental.xor
+
 var outputVal = 255
 
 class Hasher(s: String) {
-    private val sizes = s.split(regex = Regex(",\\s*")).map { it.toInt() }
+    private val sizes = s
 
-    fun hash(): Int {
-        var output = (0..outputVal).toMutableList()
+    fun realHash(): String {
+        val bytes = sizes.toByteArray(Charsets.US_ASCII) + listOf<Byte>(17,31,73,47,23)
+
         var pos = 0
         var skip = 0
-        sizes.forEach {
-            (0..(it - 1)).map { x -> x + pos }.map{ x -> output[x % output.size] }.reversed().forEachIndexed { i,v ->
-                output[(pos+i) % output.size] = v
+        var list = (0..255).toMutableList()
+        for(i in 1..64) {
+            bytes.forEach { len ->
+                (0..(len-1)).map { (it + pos) % list.size }.map { list[it] }.reversed().forEachIndexed{ i, v ->
+                    list[(pos+i) % list.size] = v
+                }
+
+                pos = (pos + skip + len) % list.size
+                skip++
+            }
+        }
+
+        return list.withIndex().groupBy { it.index / 16 }.map { it.value.map { it.value } }.map{ it.map { n -> n.toByte() } }.map { list ->
+            var o = list[0]
+            list.drop(1).forEach {
+                o = o.xor(it)
+            }
+            o
+        }.map { String.format("%02x", it) }.joinToString("")
+    }
+
+    fun hashSingle(): Int {
+        var output = (0..outputVal).toMutableList()
+        val slist = sizes.split(regex = Regex(",\\s*")).map { it.toInt() }
+        var pos = 0
+        var skip = 0
+        slist.forEach {
+            (0..(it - 1)).map { x -> x + pos }.map { x -> output[x % output.size] }.reversed().forEachIndexed { i, v ->
+                output[(pos + i) % output.size] = v
             }
 
             pos += it + skip
@@ -21,4 +50,5 @@ class Hasher(s: String) {
     }
 }
 
-fun day10p1(s: String): String = Hasher(s).hash().toString()
+fun day10p1(s: String): String = Hasher(s).hashSingle().toString()
+fun day10p2(s: String): String = Hasher(s).realHash()
